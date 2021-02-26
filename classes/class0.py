d0b8743af27def06a3925c7684cc3c2af78c7e49
zahid188 <zahidpranta5@gmail.com>
2021-02-26 12:04:15 -0500

    class0 updated.

---
+++ classes/class0.py
@@ -0,0 +1,23 @@
+# Imagine its a flight booking overflow class
+class Flight():
+    def __init__(self,capacit):
+        self.capacitys=capacit
+        self.passengers=[]
+
+    def add_passenger(self,name):
+        #if there're not any open seats
+        if not self.open_seats():
+            return  False 
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacitys - len(self.passengers)
+flight= Flight(4)
+people = ["Harry","Ron","Hermoine","zahid","raihan"]
+for person in people:
+    success= flight.add_passenger(person)
+    if success:
+        print(f"added {person} to flight succesfully")
+    else :
+        print(f"No available seats for {person}")
