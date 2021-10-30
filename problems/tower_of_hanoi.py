720be9efc2a481831070ded5e23d77f53e940987
ZAHIDUL_ALAM <zahidpranta5@gmail.com>
2021-10-30 08:56:31 +0600

    new problems

---
+++ problems/tower_of_hanoi.py
@@ -0,0 +1,13 @@
+def f2(n):
+    if n>=2:
+        f2(n//2)
+    print(n%2,end='')
+def f1(a,b):
+    if b==1:
+        return a
+    else:
+        return a+f1(a,b-1)
+        
+    
+f1(3,5)
+# f2(4)
