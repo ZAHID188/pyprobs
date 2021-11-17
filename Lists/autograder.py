0250983d31a8b20e80a522fa978a7ba04910133d
ZAHID188 <zahidpranta5@gmail.com>
2021-11-18 00:09:24 +0600

    1st problem in autograder

--- Lists/autograder.py
+++ Lists/autograder.py
@@ -4,18 +4,47 @@
 # For each word on each line check to see if the word is already in the list and 
 # if not append it to the list.
 #  When the program completes, sort and print the resulting words in alphabetical order.
+
 file=open("romeo.txt")
 main=[]
-for i in file:
-    i=i.rstrip()
-    words=i.split()
-    print(words)
-    
-    for x in range(len(words)):
-        if words[x] not in main:
-            main.append(words[x])
-        else:
+for line in file:
+    line=line.rstrip()
+    words=line.split()
+    # print(words)
+    for word in words:
+        # print(word)
+        if word in main:
             continue
+        else:
+            main.append(word)
+print(main)
+    
+
+
+
+
+
+
+# file=open("romeo.txt")
+# main=[]
+# for i in file:
+#     i=i.rstrip()
+#     words=i.split()
+#     for z in words:
+#         main.append(z)
+        
+
+# print(main)
+
+# res=[]
+# for words in main:
+#     if words not in res:
+#         res.append(words)
+# res.sort()
+# print(res)
+
 
 
         
+
+    
