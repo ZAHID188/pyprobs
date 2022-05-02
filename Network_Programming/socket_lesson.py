6ff4b11ea589e122fcee926b4c5a5043f8474608
ZAHID188 <zahidpranta5@gmail.com>
2022-05-02 17:02:53 +0600

    Socket modified

--- Network_Programming/socket_lesson.py
+++ Network_Programming/socket_lesson.py
@@ -9,12 +9,12 @@ serves as an endpoint for sending and receiving data across the network.
 # mysock.connect( ('data.pr4e.org',80) )
 
 '''
-http
+http request using socket
 '''
 import socket 
 mysock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org',80))
-cmd='GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+cmd='GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 while True:
     data=mysock.recv(512)
